0bb1db3a6de6c6225b0b13f77e15879d2939bac0
Shane Power <20102592@mail.wit.ie>
2024-09-18 15:08:07 +0100

    basic entries

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tkinter import *
 
 grid = [[9,0,0,0,2,0,1,0,0],
         [0,8,0,0,0,0,9,6,0],
@@ -9,6 +10,20 @@ grid = [[9,0,0,0,2,0,1,0,0],
         [0,7,5,0,0,0,0,0,4],
         [0,0,0,1,0,0,0,0,2],
         [0,0,0,0,7,0,0,8,0]]
+
+# window!
+root = Tk()
+root.title("Sudoku Solver")
+root.geometry("500x500")
+entry0_0 = Entry(root)
+entry0_1 = Entry(root)
+
+entry0_0.grid(row = 0, column = 0, pady = 2)
+entry0_1.grid(row = 0, column = 1, pady = 2)
+
+# for x in range(0:10)
+#     for y in range(0:10)
+#     entry
 
 
 def possible(row, column, number):
@@ -50,4 +65,5 @@ def solve():
     print(np.matrix(grid))
     input('[Enter] More possible solutions')
 
+root.mainloop
 solve()
